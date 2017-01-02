2b286680de99cb25618107bfbf0ecdbf60d5c333
Ruben Stranders <r.stranders@gmail.com>
2017-01-03 00:31:11 +0100

    Based off of happy_seed: base, splash, devise, roles, facebook, github, googleoauth, twitter, admin, jazz_hands included

---
+++ app/controllers/splash_controller.rb
@@ -0,0 +1,50 @@
+class SplashController < ApplicationController
+  skip_before_action :authenticate
+
+  def index
+  end
+
+  def signup
+    if ENV['MAILCHIMP_SPLASH_SIGNUP_LIST_ID'].blank? or ENV['MAILCHIMP_API_KEY'].blank?
+
+      @message = "The MAILCHIMP_API_KEY and MAILCHIMP_SPLASH_SIGNUP_LIST_ID environment variables need to be set for mailing list signup to work! If you don't want this feature, you can just remove the mailing list signup feature from app/views/splash/index.html.haml"
+
+    else
+      begin
+
+        gb = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'])
+
+       gb.lists(ENV['MAILCHIMP_SPLASH_SIGNUP_LIST_ID']).members.create(
+         body: {
+           email_address: params[:signup_email],
+           status: "pending"
+           }
+         )
+
+        @message = 'Thanks for signing up!'
+
+      rescue StandardError => e
+        @error = true
+
+        if e.message =~ /is already subscribed to the list/
+          @message = "You're already signed up to receive updates. Thanks for your enthusiasm!"
+        else
+          @message = "Oops! Looks like we ran into an error. How embarrassing. Please try again later!"
+        end
+
+      end
+    end
+  end
+
+
+  private
+
+    def gem_available?(name)
+       Gem::Specification.find_by_name(name)
+    rescue Gem::LoadError
+       false
+    rescue
+       Gem.available?(name)
+    end
+
+end
---
+++ spec/controllers/splash_controller_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe SplashController, :type => :controller do
+  before :each do
+    begin
+      Devise
+      sign_out :user
+    rescue NameError
+    end
+  end
+
+  after do
+    ENV['HTTP_AUTH_USERNAME'] = nil
+    ENV['HTTP_AUTH_PASSWORD'] = nil
+  end
+
+  it "should return the index page" do
+    get :index
+    expect(response).to render_template( :index )
+  end
+
+  it "should not request authentication even if http_auth is set" do
+    ENV['HTTP_AUTH_USERNAME'] = "user"
+    ENV['HTTP_AUTH_PASSWORD'] = "pass"
+
+    get :index
+    expect(response).to render_template( :index )
+
+    ENV['HTTP_AUTH_USERNAME'] = nil
+    ENV['HTTP_AUTH_PASSWORD'] = nil
+  end
+
+  context "with views" do
+    render_views
+    it "should return the index page with the correct google tracking code" do
+      ENV['GOOGLE_ANALYTICS_SITE_ID'] = '123456'
+
+      get :index
+      expect( response.body ).to include( "ga('create', '123456'" );
+
+      ENV['GOOGLE_ANALYTICS_SITE_ID'] = nil
+    end
+  end
+
+  context "mailing list signup" do
+    it "should require mailchimp env to be setup" do
+      ENV["MAILCHIMP_SPLASH_SIGNUP_LIST_ID"] = nil
+      ENV["MAILCHIMP_API_KEY"] = nil
+
+      post :signup, xhr: true
+
+      expect( assigns( :message) ).to include( "environment variables need to be set" )
+    end
+
+    it "should talk to mail chimp if the ENV is set" do
+      ENV["MAILCHIMP_SPLASH_SIGNUP_LIST_ID"] = "1"
+      ENV["MAILCHIMP_API_KEY"] = "1"
+      # assign( :gibbon_api, double( "API" ) )
+
+      stub_request(:post, "https://api.mailchimp.com/2.0/lists/subscribe").
+        with(:body => "{\"apikey\":\"1\",\"id\":\"1\",\"email\":{\"email\":null},\"double_optin\":true}").
+        to_return(:status => 200, :body => "", :headers => {})
+
+      
+      post :signup, xhr: true, params: { email: "wschenk@gmail.com" }
+
+      expect( assigns( :message) ).not_to include( "environment variables need to be set" )
+
+      ENV["MAILCHIMP_SPLASH_SIGNUP_LIST_ID"] = nil
+      ENV["MAILCHIMP_API_KEY"] = nil
+
+    end
+  end
+end
