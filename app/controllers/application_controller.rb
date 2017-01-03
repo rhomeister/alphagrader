d67362934021b02f1dd321629fb62018a6db4c00
Ruben Stranders <r.stranders@gmail.com>
2017-01-03 03:15:28 +0100

    Fix ActiveAdmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   helper_method :page_title
   def page_title
   end
+
+  def access_denied(exception)
+    redirect_to root_path, alert: exception.message
+  end
+
+  def authenticate_admin_user!
+    redirect_to root_path unless current_user.try(:admin?)
+  end
 end
