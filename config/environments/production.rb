7af4a3689d158c51bd17d8f2585ef67833ccf4a3
Ruben Stranders <r.stranders@gmail.com>
2017-01-09 03:13:55 +0100

    Allow websocket connections from primary domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://#{ENV.fetch('DOMAIN_NAME')}/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://#{ENV.fetch('DOMAIN_NAME')}/cable"
+  config.action_cable.allowed_request_origins = [ "https://#{ENV.fetch('DOMAIN_NAME')}", /https:\/\/#{ENV.fetch('DOMAIN_NAME')}.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
