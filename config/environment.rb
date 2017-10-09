c8cfa2045e46dc88b7efb4c428d643b963fcbf5e
Ruben Stranders <r.stranders@gmail.com>
2017-10-09 04:12:57 +0100

    Move away from Sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,10 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['SMTP_USERNAME'],
+  password: ENV['SMTP_PASSWORD'],
   domain: 'alphagrader.com',
-  address: 'smtp.sendgrid.net',
+  address: ENV['SMTP_SERVER'],
   port: 587,
   authentication: :plain,
   enable_starttls_auto: true
