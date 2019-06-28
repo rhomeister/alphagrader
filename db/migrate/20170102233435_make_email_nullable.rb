c181196d344b4f620c0c4e11c5a125b7e0714827
Ruben Stranders <r.stranders@gmail.com>
2019-06-28 23:48:37 +0100

    Updates for circle CI 2

--- db/migrate/20170102233435_make_email_nullable.rb
+++ db/migrate/20170102233435_make_email_nullable.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MakeEmailNullable < ActiveRecord::Migration
+class MakeEmailNullable < ActiveRecord::Migration[5.0]
   def change
     change_column_null :users, :email, true
     remove_index :users, :email
--- db/migrate/20170102233955_add_secret_token_to_identity.rb
+++ db/migrate/20170102233955_add_secret_token_to_identity.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddSecretTokenToIdentity < ActiveRecord::Migration
+class AddSecretTokenToIdentity < ActiveRecord::Migration[5.0]
   def change
     add_column :identities, :secrettoken, :string
   end
