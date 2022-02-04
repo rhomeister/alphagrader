344f9eb0ee1179512b19dbe44e72b73305d86302
carlosahs <tacahs12@gmail.com>
2022-02-04 15:33:59 -0600

    Modify condition to display loading spinner

--- app/decorators/submission_decorator.rb
+++ app/decorators/submission_decorator.rb
@@ -24,7 +24,7 @@ class SubmissionDecorator < Draper::Decorator
   end
 
   def status
-    return h.content_tag(:i, '', class: 'fa fa-spinner fa-spin fa-2x') if submission.checks_completed?
+    return h.content_tag(:i, '', class: 'fa fa-spinner fa-spin fa-2x') if !submission.checks_completed?
 
     label_class =
       case object.status
